dfa15ac8d97e573da1bef8f68aa4ad95d5b4294b
Souvik Halder <souvik.halder2020@gift.edu.in>
2023-02-20 23:40:40 +0530

    frontend setup done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Footer from "./components/layout/Footer";
+import Header from "./components/layout/Header";
+import {BrowserRouter as Router,Route,Routes} from 'react-router-dom';
+import Home from './components/Home/Home.js'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<>
+<Header/>
+<Router>
+  <Routes>
+  <Route path="/" element={<Home/>}/>
+  </Routes>
+</Router>
+<Footer/>
+</>
   );
 }
 
